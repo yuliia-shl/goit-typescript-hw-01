7fae4a911a24f7bd739d644edfc152ebca2f91f0
Yuli <yu.expert@gmail.com>
2024-12-24 01:41:16 +0200

    Add task basic 6

---
+++ src/basic/6.ts
@@ -0,0 +1,27 @@
+// Створіть інтерфейс User для типізації об'єктів, які містять такі властивості. Зверніть увагу, що адреса є необов'язковою властивістю.
+
+interface User {
+  name: string
+  age: number
+  email: string
+  address?: {
+    city: string
+    country: string
+  }
+}
+
+const mango: User = {
+  name: "Mango",
+  age: 30,
+  email: "john@example.com",
+  address: {
+    city: "New York",
+    country: "USA",
+  },
+}
+
+const poly: User = {
+  name: "Mango",
+  age: 30,
+  email: "john@example.com",
+}
