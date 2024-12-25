60ae9fce3b33d2a1dd0afc9092bc189912123dd4
Yuli <yu.expert@gmail.com>
2024-12-25 19:58:12 +0200

    Add task 3 generics

---
+++ src/generics/3.ts
@@ -0,0 +1,9 @@
+// Завдання У вас є функція merge, яка поєднує два об'єкти. Використовуйте
+// generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
+// function merge (objA, objB) {
+//   return Object.assign(objA, objB);
+// }
+
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign({}, objA, objB)
+}
